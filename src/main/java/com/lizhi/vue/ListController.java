2b3f9834555cae7603db19900c36f45ca8e2214f
lizhi <lizhi1026@126.com>
2019-04-30 19:20:14 +0800

    add content

--- src/main/java/com/lizhi/vue/ListController.java
+++ src/main/java/com/lizhi/vue/ListController.java
@@ -1,7 +1,13 @@
 package com.lizhi.vue;
 
+import com.lizhi.vue.common.Result;
+import com.lizhi.vue.entity.Item;
+import com.lizhi.vue.entity.Order;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/list")
@@ -9,6 +15,13 @@ public class ListController {
 
     @RequestMapping("/doneList")
     public String getDoneList(int startIndex, int pageSize){
-        return "";
+        Order order = new Order();
+        List <Item> list = new ArrayList<>();
+
+        order.setList(list);
+        order.setHasMore(true);
+
+
+        return Result.success(order).toJsonString();
     }
 }
