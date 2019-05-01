b792bb4079ae78dda53d1bd60726b5a45d23a251
lizhi <lizhi1026@126.com>
2019-05-01 10:18:49 +0800

    remove component

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,20 +1,26 @@
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
+(function () {
 
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!',
-      groceryList: [
-        { id: 0, text: 'Vegetables' },
-        { id: 1, text: 'Cheese' },
-        { id: 2, text: 'Whatever else humans are supposed to eat' }
-      ]
-    },
-    mounted() {
-        this.message = ' app mounted'
-    },
-  })
-  
+    Vue.component('todo-item', {
+        props: ['todo'],
+        template: '<li>{{ todo.text }}</li>'
+    })
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!',
+            groceryList: [
+                { id: 0, text: 'Vegetables' },
+                { id: 1, text: 'Cheese' },
+                { id: 2, text: 'Whatever else humans are supposed to eat' },
+                { id: 3, text: '1111' }
+            ]
+        },
+
+        mounted() {
+            this.message = ' app 1111'
+        },
+    })
+
+
+})()
