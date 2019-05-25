7ebf7e849d5f8f7f81f39ee4690a102ceb92f929
lizhi <lizhi1026@126.com>
2019-05-26 00:07:49 +0800

    add system code

---
+++ backend-system/src/main/java/me/lizhi/modules/security/service/JwtUserDetailsService.java
@@ -0,0 +1,59 @@
+package me.lizhi.modules.security.service;
+
+import me.lizhi.exception.BadRequestException;
+import me.lizhi.modules.system.domain.*;
+import me.lizhi.modules.security.security.JwtUser;
+import me.lizhi.modules.system.service.UserService;
+import me.lizhi.modules.system.service.dto.DeptDTO;
+import me.lizhi.modules.system.service.dto.JobDTO;
+import me.lizhi.modules.system.service.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+
+/**
+ * @author jie
+ * @date 2018-11-22
+ */
+@Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true, rollbackFor = Exception.class)
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtPermissionService permissionService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username){
+
+        UserDTO user = userService.findByName(username);
+        if (user == null) {
+            throw new BadRequestException("账号不存在");
+        } else {
+            return createJwtUser(user);
+        }
+    }
+
+    public UserDetails createJwtUser(UserDTO user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getAvatar(),
+                user.getEmail(),
+                user.getPhone(),
+                Optional.ofNullable(user.getDept()).map(DeptDTO::getName).orElse(null),
+                Optional.ofNullable(user.getJob()).map(JobDTO::getName).orElse(null),
+                permissionService.mapToGrantedAuthorities(user),
+                user.getEnabled(),
+                user.getCreateTime(),
+                user.getLastPasswordResetTime()
+        );
+    }
+}
---
+++ backend-system/src/main/java/me/lizhi/modules/system/service/RoleService.java
@@ -0,0 +1,81 @@
+package me.lizhi.modules.system.service;
+
+import me.lizhi.modules.system.domain.Menu;
+import me.lizhi.modules.system.domain.Role;
+import me.lizhi.modules.system.service.dto.RoleDTO;
+import me.lizhi.modules.system.service.dto.RoleSmallDTO;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author jie
+ * @date 2018-12-03
+ */
+@CacheConfig(cacheNames = "role")
+public interface RoleService {
+
+    /**
+     * get
+     * @param id
+     * @return
+     */
+    @Cacheable(key = "#p0")
+    RoleDTO findById(long id);
+
+    /**
+     * create
+     * @param resources
+     * @return
+     */
+    @CacheEvict(allEntries = true)
+    RoleDTO create(Role resources);
+
+    /**
+     * update
+     * @param resources
+     */
+    @CacheEvict(allEntries = true)
+    void update(Role resources);
+
+    /**
+     * delete
+     * @param id
+     */
+    @CacheEvict(allEntries = true)
+    void delete(Long id);
+
+    /**
+     * key的名称如有修改，请同步修改 UserServiceImpl 中的 update 方法
+     * findByUsers_Id
+     * @param id
+     * @return
+     */
+    @Cacheable(key = "'findByUsers_Id:' + #p0")
+    List<RoleSmallDTO> findByUsers_Id(Long id);
+
+    @Cacheable(keyGenerator = "keyGenerator")
+    Integer findByRoles(Set<Role> roles);
+
+    /**
+     * updatePermission
+     * @param resources
+     * @param roleDTO
+     */
+    @CacheEvict(allEntries = true)
+    void updatePermission(Role resources, RoleDTO roleDTO);
+
+    /**
+     * updateMenu
+     * @param resources
+     * @param roleDTO
+     */
+    @CacheEvict(allEntries = true)
+    void updateMenu(Role resources, RoleDTO roleDTO);
+
+    @CacheEvict(allEntries = true)
+    void untiedMenu(Menu menu);
+}
